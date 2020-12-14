2b15919384135f53b9b4c9c199ea050679ec3541
VDHUNG79 <64667768+VDHUNG79@users.noreply.github.com>
2020-12-14 09:01:38 +0700

    redirect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import SinglePost from './components/SinglePost';
+import Post from './components/Post';
+import Project from './components/Project';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/post/:slug" component={SinglePost} />
+        <Route path="/post" component={Post} />
+        <Route path="/project" component={Project} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import image from '../images/destination//sapa-3.png';
+
+export default function Home() {
+  return (
+    <main>
+      <img src={image} alt="Mountain View" className="absolute object-cover w-full h-full"/>
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1
+          className="text-6xl text-gray-100 font-bold cursive"
+          /*
+          className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"
+          */
+        >
+          Hello. I'm Vu Duc Hung
+        </h1>
+      </section>
+    </main>
+  )
+}
